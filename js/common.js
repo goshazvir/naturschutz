b7222a94608d7e8a9506ded650b290890290d9c7
Georgiy <goshazvir@gmail.com>
2014-10-07 23:39:53 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,10 @@
 head.ready(function() {
-
+	$('.js-nav-mobile').on('click', function() {
+	 $( ".nav" ).slideToggle( "slow", function() {
+	     // Animation complete.
+	   });
+	});
+	$('.nav-list-item').on('click', function() {
+	 $(this).toggleClass("is-drop");
+	});
 });
